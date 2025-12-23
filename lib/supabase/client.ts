35b6ee96e7737ad6c4190303f3e92ced28a205f8
kurakuro92 <65663793+kurakuro92@users.noreply.github.com>
2025-12-23 17:20:02 +0900

    lib/supabase/client.ts cookie

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,7 +3,6 @@ import { createBrowserClient } from "@supabase/ssr";
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    // ここを ANON_KEY から PUBLISHABLE_KEY に変更して、Vercel設定と合わせます
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
